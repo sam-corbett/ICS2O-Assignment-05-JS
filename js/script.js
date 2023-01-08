15ad9f93b37c4cc3c50cd647f749ec8abd5ee484
Sam Corbett <113055935+sam-corbett@users.noreply.github.com>
2023-01-08 21:07:22 +0000

    big change in code, WIP

--- js/script.js
+++ js/script.js
@@ -16,20 +16,31 @@ if (navigator.serviceWorker) {
 }
 
 /**
- * This function displays an alert.
+ * This function displays the answers for each equation!
  */
 function myButtonClicked() {
-  var numA = parseFloat(document.getElementById("num-a").value)
-  var numB = parseFloat(document.getElementById("num-b").value)
-  var numC = parseFloat(document.getElementById("num-c").value)
+  var numAdd1 = parseFloat(document.getElementById("variable-addition-1").value)
+  var numAdd2 = parseFloat(document.getElementById("variable-addition-2").value)
+  var numMulti1 = parseFloat(document.getElementById("variable-multiply-1").value)
+  var numMulti2 = parseFloat(document.getElementById("variable-multiply-2").value)
 
-  var product = numC
+  var productAdd = numAdd1 + numAdd2
+  var productMulti = 0
   var counter = 0
-
-  while (counter < Math.abs(numB)) {
+  
+  document.getElementById("evaluate-equations").innerHTML = "Both numbers add up to " + productAdd + "."
+  
+  while (counter < Math.abs(numMulti2)) {
     counter++
-    product = product + Math.abs(numB)
-    document.getElementById("x-values").innerHTML += " x = " + counter + "</br>"
-    document.getElementById("y-values").innerHTML += " y = " + product + "</br>"
+    productMulti = productMulti + Math.abs(numMulti1)
   }
+
+  
+  if ((numMulti1 < 0 && numMulti2 > 0) || (numMulti2 < 0 && numMulti1 > 0))
+    document.getElementById("evaluate-equations").innerHTML =
+      "Both numbers multiply to -" + productMulti + "."
+  else
+    document.getElementById("evaluate-equations").innerHTML =
+      "Both numbers multiply to " + productMulti + "."
+
 }
