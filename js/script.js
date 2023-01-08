caacf237be2efe736e9942733af5dd3be4c30450
sam-corbett <sam-corbett@users.noreply.github.com>
2023-01-08 21:12:59 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -21,26 +21,29 @@ if (navigator.serviceWorker) {
 function myButtonClicked() {
   var numAdd1 = parseFloat(document.getElementById("variable-addition-1").value)
   var numAdd2 = parseFloat(document.getElementById("variable-addition-2").value)
-  var numMulti1 = parseFloat(document.getElementById("variable-multiply-1").value)
-  var numMulti2 = parseFloat(document.getElementById("variable-multiply-2").value)
+  var numMulti1 = parseFloat(
+    document.getElementById("variable-multiply-1").value
+  )
+  var numMulti2 = parseFloat(
+    document.getElementById("variable-multiply-2").value
+  )
 
   var productAdd = numAdd1 + numAdd2
   var productMulti = 0
   var counter = 0
-  
-  document.getElementById("evaluate-equations").innerHTML = "Both numbers add up to " + productAdd + "."
-  
+
+  document.getElementById("evaluate-equations").innerHTML =
+    "Both numbers add up to " + productAdd + "."
+
   while (counter < Math.abs(numMulti2)) {
     counter++
     productMulti = productMulti + Math.abs(numMulti1)
   }
 
-  
   if ((numMulti1 < 0 && numMulti2 > 0) || (numMulti2 < 0 && numMulti1 > 0))
     document.getElementById("evaluate-equations").innerHTML =
       "Both numbers multiply to -" + productMulti + "."
   else
     document.getElementById("evaluate-equations").innerHTML =
       "Both numbers multiply to " + productMulti + "."
-
 }
